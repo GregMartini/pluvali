9b6e3912352e978e417d441a4be698ccfb1e9b37
Greg <Gregory.martini@ucdenver.edu>
2014-11-06 15:25:59 -0700

    login page semi-working
    I was able to make the login page linked to the database (kind of). It
    doesn't redirect properly when login works/fails.

--- Django/pluvali/CopingGame/urls.py
+++ Django/pluvali/CopingGame/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, patterns
+from django.contrib.auth.views import login
 from CopingGame import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.index, name='/accounts/index'),
 	url(r'^(?P<sceneID>\d+)/$', views.game, name='game'),
+	url(r'^login/', 'CopingGame.views.login', name='login'),
 ]
--- Django/pluvali/CopingGame/views.py
+++ Django/pluvali/CopingGame/views.py
@@ -1,16 +1,38 @@
-#from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
-#from django.http import Http404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render, get_object_or_404
+from django.core.urlresolvers import reverse_lazy
+from django.core.context_processors import csrf
 
 from CopingGame.models import User, Scenario
 
-#link for scenario index
-def index(request):
-    scenario_list = Scenario.objects.order_by('title')
-    context = {'scenario_list': scenario_list}
-    return render(request, 'CopingGame/index.html', context)
+
+def login(request):
+	logout(request)
+	username = password = ''
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return HttpResponseRedirect('index')
+	context = { 'username':username}
+	context.update(csrf(request))
+	return render(request, 'registration/login.html', context)
 	
-#link for game page	
+#link for scenario index
+#@login_required
+def index(request):
+#	if not request.user.is_authenticated():
+#		return redirect(request, 'CopingGame/login_page.html')
+	scenario_list = Scenario.objects.order_by('title')
+	context = {'scenario_list': scenario_list}
+	return render(request, 'CopingGame/index.html', context)
+	
+#link for game page
+@login_required	
 def game(request, sceneID):
 	scene = get_object_or_404(Scenario, pk = sceneID)
 	return render(request, 'CopingGame/game_page.html', {'scene': scene})
