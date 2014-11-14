6f1692ed86ade1d2d01426e25eda6053497aef7d
TimLeikam <timothy.leikam@ucdenver.edu>
2014-11-13 21:45:41 -0700

    Revert "REVERT"
    This reverts commit 1d4471a6449cbfeacd96917428a2c38d81deb2ea.

--- pluvali/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^CopingGame/', include('CopingGame.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-	url(r'^login/$', 'django.contrib.auth.views.login'),
-	url(r'^logout/$', 'django.contrib.auth.views.logout',{'next_page': '/login/'})
-]
