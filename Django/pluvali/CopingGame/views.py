8f5ccde0ef8bfccbbd43be81bb6b9f251e93c252
Greg <Gregory.martini@ucdenver.edu>
2014-11-20 21:47:02 -0700

    Points work
    -The game page now adds one point when a PLAYER selects one of the three
    options. I will next make the game page link through multiple problems
    and finally to a victory screen.
    -I fixed the registration page so it links newly made USERS to a new
    PLAYER, attaches email, points, and default text and background color as
    hex int. (the two colors currently don't effect anything).

--- Django/pluvali/CopingGame/views.py
+++ Django/pluvali/CopingGame/views.py
@@ -16,45 +16,52 @@ from django.contrib.auth.models import User
 #link for homepage, requires login
 @login_required(login_url='/login')
 def index(request):
-	return render(request, 'CopingGame/index.html')
+	player = Player.objects.get(user=User.objects.get(username=request.user))
+	context = {'player':player}
+	return render(request, 'CopingGame/index.html', context)
 
 #link for help page
 def help(request):
-	return render(request, 'CopingGame/help_page.html')
+	player = Player.objects.get(user=User.objects.get(username=request.user))
+	context = {'player':player}
+	return render(request, 'CopingGame/help_page.html', context)
 
 #link for scenario index
 @login_required(login_url='/login')
 def scenario_index(request):
+	player = Player.objects.get(user=User.objects.get(username=request.user))
 	scenario_list = Scenario.objects.order_by('title')
-	context = {'scenario_list': scenario_list}
+	context = {'scenario_list': scenario_list, 'player':player}
 	return render(request, 'CopingGame/scenario_index.html', context)
 	
 #link for game page
 @login_required(login_url='/login')
 def game(request, sceneID):
+	player = Player.objects.get(user=User.objects.get(username=request.user))
 	scene = get_object_or_404(Scenario, pk = sceneID)
 	problems = scene.problems
 	solutions = scene.solutions
-	context = {'scene':scene, 'problems':problems, 'solutions':solutions}
 	if request.method == 'POST':
-		p = Player.objects.get(user=request.user)
-		p.points += 1
-		
+		player.points += 1
+		player.save()
+	context = {'scene':scene, 'problems':problems, 'solutions':solutions, 'player':player}	
 	return render(request, 'CopingGame/game_page.html', context)
 
 @login_required(login_url='/login')
 def store(request):
+	player = Player.objects.get(user=User.objects.get(username=request.user))
 	items_list = Store.objects.order_by('itemName')
-	context = {'items_list':items_list}
+	context = {'items_list':items_list, 'player':player}
 	return render(request, 'CopingGame/store_page.html', context)
 
 #User registration
 def register(request):
 	if request.method == 'POST':
 		form = UserCreationForm(request.POST)
+		player_email = request.POST['email']
 		if form.is_valid():
 			new_user = form.save()
-			
+			new_player = Player.objects.create(user=User.objects.get(username=request.POST['username']), email=player_email)
 			new_user = authenticate(username=request.POST['username'], password=request.POST['password1'])
 			return HttpResponseRedirect("/CopingGame/")
 		else:
