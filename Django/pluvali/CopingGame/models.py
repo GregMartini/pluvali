b081b5a94db21665a769ed41e58524261542b1b5
Greg <Gregory.martini@ucdenver.edu>
2014-11-23 12:38:11 -0700

    imagefields added
    Slight changes made to add in the images for each problem/solution, they
    don't work yet, but I think it's on the right path.

--- Django/pluvali/CopingGame/models.py
+++ Django/pluvali/CopingGame/models.py
@@ -10,7 +10,7 @@ class Player(models.Model):
 	user = models.OneToOneField(User)
 	email = models.CharField(max_length=30)
 	points = models.IntegerField(default=0)
-	#avatarPic = FileField(upload_to=get_image_path, blank=True, null=True)
+	#avatarPic = models.ImageField(upload_to=get_image_path, blank=True, null=True)
 	fav_bg = models.IntegerField(default=defs.BG_WHITE)
 	fav_text = models.IntegerField(default=defs.TEXT_BLACK)
 	stage = models.IntegerField(default=0) #used for iterating through scenarios
@@ -19,14 +19,14 @@ class Player(models.Model):
 	pass
 		
 class Solutions(models.Model):
-	#media = FileField(upload_to=get_image_path, blank=True, null=True)
+	pictureS = models.ImageField(upload_to='/media/solutions/', blank=True, null=True)
 	solution = models.CharField(max_length=300, default="Solution")
 	def __str__(self):
 		return self.solution
 	pass
 
 class Problems(models.Model):
-	#media = FileField(upload_to=get_image_path, blank=True, null=True)
+	pictureP = models.ImageField(upload_to='/media/problems/', blank=True, null=True)
 	problem = models.CharField(max_length=225, default="The Problem.")
 	solutions = models.ManyToManyField(Solutions)
 	def __str__(self):
