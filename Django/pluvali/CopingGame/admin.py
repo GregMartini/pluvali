6fe1f024b4b9f591a94edef0018d50b245fe1a3f
alexanderdavis <alexanderdavis@users.noreply.github.com>
2014-12-01 10:45:55 -0700

    Update admin.py
    Included app administration in default admin page.

--- Django/pluvali/CopingGame/admin.py
+++ Django/pluvali/CopingGame/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from CopingGame.models import *
 
 # Register your models here.
+admin.site.register(Player)
+admin.site.register(Solutions)
+admin.site.register(Problems)
+admin.site.register(Scenario)
+admin.site.register(Store)
+admin.site.register(Purchases)
