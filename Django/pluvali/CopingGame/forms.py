3d65c956d0dd4ce220deefb1d75711bd2588ce17
Greg <Gregory.martini@ucdenver.edu>
2015-02-26 16:07:47 -0700

    Profile page 95%
    The purchases are now showing correctly and the player can upload a
    picture now. However, I think due to no size limit, the picture doesn't
    show if too large. Not a hundred percent sure on this theory.

--- Django/pluvali/CopingGame/forms.py
+++ Django/pluvali/CopingGame/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.models import User
-#from django.core import validators
 from CopingGame.models import Player
 
 class PlayerProfileForm(forms.ModelForm):
