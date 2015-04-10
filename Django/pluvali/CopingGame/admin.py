e2994dd727e5a82df8da0749d742ad2e276b0664
alexanderdavis <alexanderdavis@users.noreply.github.com>
2015-04-10 05:53:16 -0600

    Added Inlines to the admin page
    Edits make managing many-to-many relations much easier.  However, one-to-one relationships are left as is because you can still edit them from the default page.

--- Django/pluvali/CopingGame/admin.py
+++ Django/pluvali/CopingGame/admin.py
@@ -1,11 +1,31 @@
 from django.contrib import admin
 from CopingGame.models import *
+class SolutionsInline(admin.TabularInline):
+	model = Problems.solutions.through
+	extra = 3
+class ProblemAdmin(admin.ModelAdmin):
+	inlines = [SolutionsInline]
+	exclude = ('solutions',)
+
+class ProblemsInline(admin.TabularInline):
+	model = Scenario.problems.through
+	extra = 3
+class Player_listInline(admin.TabularInline):
+	model = Scenario.player_list.through
+	extra = 3
+class Group_listInline(admin.TabularInline):
+	model = Scenario.group_list.through
+	extra = 3
+class ScenarioAdmin(admin.ModelAdmin):
+	inlines = [ProblemsInline,Player_listInline,Group_listInline]
+	exclude = ('problems','player_list','group_list')
+
 
 admin.site.register(Player)
 admin.site.register(PlayerGroup)
+
 admin.site.register(Solutions)
-admin.site.register(Problems)
-admin.site.register(Scenario)
+admin.site.register(Problems,ProblemAdmin)
+admin.site.register(Scenario, ScenarioAdmin)
 admin.site.register(Store)
 admin.site.register(Purchases)
-
