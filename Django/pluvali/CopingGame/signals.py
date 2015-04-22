1a363b7c81b1a20d14e4c3f595f861290f6ec1fd
Greg <Gregory.martini@ucdenver.edu>
2015-04-22 12:06:29 -0600

    changes in signals.py
    I forgot last night to add the if new statement, so that the purchases
    are created only when new store items are made.

--- Django/pluvali/CopingGame/signals.py
+++ Django/pluvali/CopingGame/signals.py
@@ -5,7 +5,8 @@ from CopingGame.models import Player, Store, Purchase
 
 @receiver(post_save, sender=Store)
 def model_post_save(sender, instance, created, **kwargs):
-	players_list = list(Player.objects.all())
-	for p in players_list:
-		purchase = Purchase.objects.create(player=p, itemFKey=instance, owned=False)
-		purchase.save()
+	if created:
+		players_list = list(Player.objects.all())
+		for p in players_list:
+			purchase = Purchase.objects.create(player=p, itemFKey=instance, owned=False)
+			purchase.save()
